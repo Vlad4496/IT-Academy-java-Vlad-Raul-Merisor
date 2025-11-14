3123f8805ff34280e03b0e4022b88d9a5fb6b7c5
Vlad4496 <vlad_4496@yahoo.com>
2025-11-14 11:41:38 +0200

    finished the challenge from 10 to 20 completing the course3

---
+++ Curs3/src/Curs3/Challenge12.java
@@ -0,0 +1,12 @@
+package Curs3;
+
+public class Challenge12 {
+    public static void main(String[] args) {
+        int number = 25;
+
+        int negativeNumber = -number;
+
+        System.out.println(number);
+        System.out.println(negativeNumber);
+    }
+}
